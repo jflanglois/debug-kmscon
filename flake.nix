7004cc0a8da35480ebe398ab5827acaf4fe0acf1
Julien Langlois <yourstruly@julienlanglois.me>
2025-01-06 10:41:12 -0500

    Fixed by https://github.com/Aetf/kmscon/pull/96

--- flake.nix
+++ flake.nix
@@ -7,6 +7,19 @@
       system = "x86_64-linux";
       modules = [
         ({ config, pkgs, lib, ... }: {
+          nixpkgs.overlays = [
+            (final: prev: {
+              kmscon = prev.kmscon.overrideAttrs (old: {
+                patches = old.patches ++ [
+                  (pkgs.fetchpatch {
+                    name = "0002-fix-sigchld.patch";
+                    url = "https://github.com/Aetf/kmscon/commit/a28e134de089447d9c78f2054db4f0be18878669.patch";
+                    sha256 = "sha256-uXjI4hhf42YvhqsA9LJLzbe0goHjcKJrITtux9dq6jg=";
+                  })
+                ];
+              });
+            })
+          ];
           services.kmscon = {
             enable = true;
             autologinUser = "root";
